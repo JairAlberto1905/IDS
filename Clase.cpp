d4ad8230ec7b4d0885fb0faf8c9430250509c48a
Alejandr0Marquez <a21310355@ceti.mx>
2021-11-15 19:57:04 -0600

    Se agrego el switch y los casos para asi terminar el programa

--- Clase.cpp
+++ Clase.cpp
@@ -22,3 +22,67 @@ int main()
     			printf("\n\nIngresa la opci%cn aqu%c: ", 162, 161);
     			scanf("%i", &opcion); 
 				   /* agregar desde el switch hasta el return 0*/
+				switch (opcion)
+    			{
+        			case 1:
+        				system("cls");
+    					printf("Ingresa la cantidad a depositar: $");
+    					scanf("%f", &deposito);
+    					saldo=saldo+deposito;
+    					printf("\nDeposito exitoso\n\n");
+    					system("pause");
+    					system("cls");
+    					break;
+    	
+    				case 2:
+    					system("cls");
+    					printf("Ingresa la cantidad a retirar: $");
+    					scanf("%f", &retiro);
+    					if(retiro>saldo)
+    					{
+    						system("cls");
+    						printf("Fondos insuficientes\n\n");
+    						system("pause");
+    						system("cls");
+						}
+						else
+						{
+							saldo=saldo-retiro;
+							printf("\nRetiro exitoso\n\n");
+    						system("pause");
+    						system("cls");
+						}
+    					break;
+    	
+    				case 3:
+    					system("cls");
+    					printf("Tu saldo es de: $%.2f\n\n", saldo);
+    					system("pause");
+    					system("cls");
+    					break;
+    	
+    				case 4:
+    					system("cls");
+    					printf("Bai");
+    					break;
+    	
+    				default: 
+    					system("cls");
+    					printf("Ingresa una opci%cn v%clida\n\n", 162, 160);
+    					system("pause");
+    					system("cls");
+				}	
+    		}
+			while (opcion !=4);
+			return 0;
+		}
+		else
+		{
+			system("cls");
+			printf("PIN incorrecto, ingresa el PIN nuevamente\n\n");
+			intento++;
+			system("pause");
+			system("cls");
+		}
+	}
+}
